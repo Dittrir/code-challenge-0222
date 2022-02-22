cd9dcc5b97793c95076145437917e39244ecd7b1
Robin Dittrich <robinodittrich@gmail.com>
2022-02-22 12:35:33 -0800

    Add create and show actions to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,26 @@ class UsersController < ApplicationController
   def index
   end
 
+  def show
+    @user = User.find(session[:user_id])
+  end
+
   def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to dashboard_path(user.id)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def user_params
+    params.permit(:name, :email, :password, :password_confirmation, :role, :description, :age)
   end
 end
