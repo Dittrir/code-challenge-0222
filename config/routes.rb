51d5b5358785a206a6f11958a22e89c8147c8427
Robin Dittrich <robinodittrich@gmail.com>
2022-02-22 13:24:48 -0800

    Add additional routes to allow login and logout functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   get '/dashboard', to: 'users#show'
-  
+  delete '/logout', to: 'users#logout'
+
+  get '/login', to: 'users#login_form'
+  post '/login', to: 'users#login_user'
   get '/register', to: 'users#new'
   post '/register', to: 'users#create'
 end
