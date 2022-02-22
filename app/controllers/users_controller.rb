3e023cab13dbca338d768bfd5c03df81b15a9845
Robin Dittrich <robinodittrich@gmail.com>
2022-02-22 13:27:31 -0800

    Add login user action to users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def login_user
+    user = User.find_by(email: params[:email])
+    if user == nil
+      flash[:error] = "User does not exist. Please try again."
+      redirect_to "/login"
+    elsif user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.name}!"
+      redirect_to dashboard_path
+    else
+      flash[:error] = "Email or password is incorrect. Please try again."
+      redirect_to "/login"
+    end
+  end
+
 private
 
   def user_params
