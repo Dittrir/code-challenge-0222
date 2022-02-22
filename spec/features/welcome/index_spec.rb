cb4228af85e12f1f3f48d4f8c26540ce278fe599
Robin Dittrich <robinodittrich@gmail.com>
2022-02-22 10:31:37 -0800

    Update coverage.

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Index Page', type: :feature do
+  describe 'view' do
+    it 'includes a button to create a new user' do
+      visit root_path
+
+      click_link 'Register'
+      expect(current_path).to eq("/register")
+
+      fill_in :name, with: 'Robin'
+      fill_in :email, with: 'robin@email.com'
+      fill_in :password, with: 'password12345'
+      fill_in :password_confirmation, with: 'password12345'
+      click_button 'Submit'
+
+      expect(page).to have_content("Robin's Dashboard")
+    end
+  end
+end
