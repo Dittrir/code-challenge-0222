1c6985a4f654fb51ef96c0c5b2accb9084e7b733
Robin Dittrich <robinodittrich@gmail.com>
2022-02-23 12:28:22 -0800

    Refactor posts controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,29 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.find(params[:id])
+    binding.pry
+    comment = @post.comments.new(post_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to dashboard_path(@user.id)
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.permit(:title, :body)
+    end
 end
